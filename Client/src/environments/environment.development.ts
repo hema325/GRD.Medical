9991fe247ac74ec006e4638f2a8fe06ed7adedb1
hema325 <hema24790@gmail.com>
2023-12-23 22:52:40 +0200

    fix rounded images bug in client app

--- Client/src/environments/environment.development.ts
+++ Client/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    baseUrl: 'https://localhost:7295/api',
+    baseUrl: 'https://localhost:7295/api',  // https://192.168.1.104:7295 test from other devices
     AIModelsBaseUrl: 'http://localhost:5000'
 };
