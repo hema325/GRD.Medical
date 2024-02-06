dc7742ffb9011547bbe846d4a6207aa34edac9e9
hema325 <hema24790@gmail.com>
2024-02-07 00:42:17 +0200

    Allow users to send messages in chat while others are writing

--- Client/src/environments/environment.development.ts
+++ Client/src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
-    baseUrl: 'https://localhost:7295/api',
-    hubUrl: 'https://localhost:7295/api/hubs',
-    AIModelsBaseUrl: 'https://rotary-seeks-samoa-male.trycloudflare.com',
+    baseUrl: 'api',
+    hubUrl: 'api/hubs',
+    AIModelsBaseUrl: 'http://localhost:5000',
     isProduction: false,
     defaultUserImageUrl: 'assets/images/profile.webp',
     publishablekey: 'pk_test_51OcQLeKeiitM3CTJES6pQowmssu6vlR4wazR1GD91B7gqXntQNl27wo9IBhjJkxEqcMSJLdnnaLgt2e4l4yH80At00dyr4kLtG'
